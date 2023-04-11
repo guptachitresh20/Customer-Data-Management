8c51f79aad44931acc983b87b6f3520d727087b9
Abhinandan Mishra <abhinandanm659@gmail.com>
2023-04-11 16:40:11 +0530

    added material ui components

--- cdm-app/src/app/app.module.ts
+++ cdm-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from 'src/material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {ReactiveFormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
